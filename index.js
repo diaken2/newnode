f77e8b12099a18614ab78d6ca03424c3dc940d16
diaken2 <107970623+diaken2@users.noreply.github.com>
2022-06-28 21:30:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(express.json({extended:true}))
 
 app.use('/api', require('./routes/upload.route'))
 app.use('/images',express.static(path.join(__dirname,"images")))
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'clientt/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/clientt/build/index.html'));
 });
 const POST=process.env.PORT || 3000
 app.listen(POST,()=>{
