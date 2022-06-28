cf26ff60a5f3ba926b6b67c3ea7bee5e1fa43f71
Кенан Салахоа <you@example.com>
2022-06-28 23:14:35 +0500

    first commit

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(express.json({extended:true}))
 
 app.use('/api', require('./routes/upload.route'))
 app.use('/images',express.static(path.join(__dirname,"images")))
-app.use(express.static(path.join(__dirname, 'clientt/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/clientt/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-const POST=process.env.PORT || 3000
+const POST=process.env.PORT || 5000
 app.listen(POST,()=>{
     console.log("Server has been launched...")
 })
